1568b2b25aa919c9f44a1c12e471917f29429236
John Thiriet <john.thiriet@outlook.com>
2020-06-18 14:18:17 +0200

    Add source code.

---
+++ src/AnalyticsSample/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
+
+namespace AnalyticsSample
+{
+    public partial class App : Application
+    {
+        const string AppCenteriOSKey = "<Your App Center iOS Key>";
+        const string AppCenterAndroidKey = "<Your App Center Android Key>";
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start($"ios={AppCenteriOSKey};android={AppCenterAndroidKey}", typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ src/AnalyticsSample/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Microsoft.AppCenter.Analytics;
+using Xamarin.Forms;
+
+namespace AnalyticsSample
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private string _color;
+        private string _city;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCityClicked(object sender, EventArgs e)
+        {
+            var imageButton = sender as ImageButton;
+
+            if (imageButton == parisBtn)
+                _city = "Paris";
+            else if (imageButton == beijingBtn)
+                _city = "Beijing";
+            else if (imageButton == seattleBtn)
+                _city = "Seattle";
+        }
+
+        private void OnColorButtonClicked(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+
+            _color = btn?.Text;
+        }
+
+        private void OnSubmitButtonClicked(object sender, EventArgs e)
+        {
+            if (_color == null || _city == null)
+                return;
+
+            var parameters = new Dictionary<string, string>
+            {
+                { "Color", _color },
+                { "City", _city }
+            };
+            Analytics.TrackEvent("MyEvent", parameters);
+        }
+    }
+}
